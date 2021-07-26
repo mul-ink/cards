16211b3181066e0f693d58f352179d4162636371
Adam Rybinski <adam@rybinski.info>
2021-07-26 21:51:28 +0200

    Ignore files in build

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,4 +1,14 @@
 export default {
+  exclude: [
+    '**/dist/**/*',
+    '**/LICENSE',
+    '**/package*.json',
+    '**/README.md',
+    '**/screenshot.png',
+    '**/snowpack.config.mjs',
+    '**/jest-puppeteer.config.js',
+    '**/node_modules/**/*',
+  ],
   optimize: {
     bundle: true,
     minify: true,
