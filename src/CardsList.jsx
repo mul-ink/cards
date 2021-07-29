8a194d47f77b81f6fd56ed00f58d2d4a91f4b5bd
Adam Rybinski <adam@rybinski.info>
2021-07-29 09:25:12 +0200

    Add state while revalidate requests support

--- src/CardsList.jsx
+++ src/CardsList.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import useSWR from 'swr';
 import styles from './app.module.css';
 import CardPlate from './components/creditCardPlate/Card.jsx';
 
+
 export default function CardList() {
-  const [data, setData] = React.useState([]);
-	useEffect(() => {
-		fetch('/api/cards').then((res) => res.json().then(setData))
-  }, []);
-  console.log('ddata2', data);
+  const { data, error } = useSWR('/api/cards', fetcher);
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  console.log('ddata23', data);
 	return (
 		<div className={styles.cardListLayout}>
-			{ data.map((card) => <CardPlate card={card}/>)}
+			{ data.map((card) => <CardPlate key={card.id} card={card}/>)}
 		</div>
 	);
 }
+
+const fetcher = (...args) => fetch(...args).then(res => res.json()) 
