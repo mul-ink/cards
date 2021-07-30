d08a3567544b83e5311882c6f120b571de8d1861
Adam Rybinski <arybinski@ibm.com>
2021-07-30 15:14:17 +0200

    Validate Card on Submit

--- src/components/creditCardPlate/Card.jsx
+++ src/components/creditCardPlate/Card.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import styles from './card.module.css';
-
+import VisaIcon from './VisaIcon';
+import MCIcon from './MCIcon';
 
 export default function CardPlate({ card }) {
     return (
     <div className={styles.cardContainer}>
         <div className={`${styles.cardPlate} ${styles.cardShadow}`}>
-          <p className={`${styles.flexItem} ${styles.textTop}`}>{ card.vendor }</p>
+          <p className={`${styles.flexItem} ${styles.textTop}`}><Vendor card={card}/></p>
           <p className={`${styles.flexItem} ${styles.cardNumber}`}>{ card.number }</p>
           <p className={`${styles.flexItem} ${styles.cardHolder}`}>{ card.holder }</p>
         </div>
     </div>
     )
+}
+
+const Vendor = ({ card }) => {
+  if (card?.vendor === 'VISA') return <VisaIcon />
+  if (card?.vendor === 'MC') return <MCIcon />
+  return card?.vendor;
 }
